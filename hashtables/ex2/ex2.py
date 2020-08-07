10951dfcc0ccdf0d198457445841bdf4ce2faafe
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-08-07 09:49:12 -0700

    linear implementation

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,36 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+tickets = [
+    Ticket(source="PIT", destination="ORD"),
+    Ticket(source="XNA", destination="CID"),
+    Ticket(source="SFO", destination="BHM"),
+    Ticket(source="FLG", destination="XNA"),
+    Ticket(source="NONE", destination="LAX"),
+    Ticket(source="LAX", destination="SFO"),
+    Ticket(source="CID", destination="SLC"),
+    Ticket(source="ORD", destination="NONE"),
+    Ticket(source="SLC", destination="PIT"),
+    Ticket(source="BHM", destination="FLG")
+]
 
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    stops = {}
+    route = []
+    for ticket in tickets:
+        stops[ticket.source] = ticket.destination
+
+    source = stops["NONE"]
+    route.append(source)
+    for i in range(len(stops)):
+        if i != 0:
+            route.append(stops[route[i-1]])    
+
+    print(route)
 
     return route
+
+reconstruct_trip(tickets, len(tickets))
