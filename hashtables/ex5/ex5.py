9cfa83662c8568d7ba7fcdab509f9311f98c9195
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-08-07 10:47:34 -0700

    O(n^2?)

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,15 +1,28 @@
 # Your code here
-
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    file_dict = {}
+    for path in files:
+        #split paths into directories
+        file_dirs = path.split('/')[-1]
+        #if directory already exists, append path
+        if file_dirs in file_dict:
+            file_dict[file_dirs].append(path)
+        else:
+            file_dict[file_dirs] = path
 
-    return result
-
+    results = []
+    # TODO: reduce complexity
+    #check all queries for files with matching paths
+    for query in queries:
+        if query in file_dict:
+            result = file_dict[query]            
+            for path in result:
+                results.append(path)
+    
+    return results
 
 if __name__ == "__main__":
     files = [
