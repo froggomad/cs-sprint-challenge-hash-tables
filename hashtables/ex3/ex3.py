1d2159ab98a0551de5291cafb61af180f168c489
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-08-08 23:09:51 -0700

    reduced complexity

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,16 +1,25 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # TODO: O(n)?
+    array_count = len(arrays)
+    result = []
+    unique_values = {}
+    for array in arrays:
 
+        for value in array:
+            if value in unique_values:
+                unique_values[value] += 1
+            else:                
+                unique_values[value] = 1
+
+            if unique_values[value] == array_count:
+                result.append(value)
+            
     return result
-
 
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
